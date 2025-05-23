3a213b65ac831f772da8bb2925b0383f97914095
fenroe <scrambles9494@gmail.com>
2025-05-23 21:00:41 +0300

    increased the file size limit on server

--- salinstudio-server/src/app.module.ts
+++ salinstudio-server/src/app.module.ts
@@ -3,6 +3,7 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { ArtModule } from './art/art.module';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
   imports: [
@@ -10,6 +11,11 @@ import { ArtModule } from './art/art.module';
     UserModule,
     ConfigModule.forRoot({ isGlobal: true }),
     ArtModule,
+    MulterModule.register({
+      limits: {
+        fileSize: 50 * 1024 * 1024,
+      },
+    }),
   ],
 })
 export class AppModule {}
