91e3d016486abecb51a3a5352df0d846ef12379c
fenroe <scrambles9494@gmail.com>
2025-05-23 18:14:07 +0300

    fixed an issue with database config in server

--- salinstudio-server/src/main.ts
+++ salinstudio-server/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DATABASE_CONFIG } from './config/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors();
+  console.log(DATABASE_CONFIG);
   await app.listen(3000);
 }
 void bootstrap();
