2827c3554226cfa7f0644d4235bdd829b1d60cf0
fenroe <scrambles9494@gmail.com>
2025-05-23 21:24:38 +0300

    increased the file size limit on server

--- salinstudio-server/src/main.ts
+++ salinstudio-server/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useBodyParser('json', { limit: '50mb' });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors();
   await app.listen(3000);
