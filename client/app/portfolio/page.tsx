146cf2695bad0e2891dc801326be185ebc65657f
fenroe <scrambles9494@gmail.com>
2025-05-27 21:18:29 +0300

    fixed url issue

--- client/app/portfolio/page.tsx
+++ client/app/portfolio/page.tsx
@@ -2,7 +2,7 @@ import { Art } from "@/types/Art";
 import { Portfolio } from "./Portfolio";
 
 export default async function PortfolioPage() {
-  const res = await fetch("/api/art", {
+  const res = await fetch("http://server:3000/art", {
     cache: "no-store",
   });
   const data = await res.json();
